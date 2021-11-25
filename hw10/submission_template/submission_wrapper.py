b1d970128d89d24dcca6b761684f88d5d83b1493
Hanneli <hannelita@gmail.com>
2021-11-25 17:08:17 -0500

    HW10 template

---
+++ hw10/submission_template/submission_wrapper.py
@@ -0,0 +1,32 @@
+import argparse
+import os, sys
+from zipfile import ZipFile
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-id', help='Student ID (only numbers)', required=True)
+args = parser.parse_args()
+STUDENTID = '{}'.format(args.id) 
+
+def main():
+    print("Preparing files to zip")
+    src_folder = os.path.join('.', 'images')
+    
+    
+    to_zip = [src_folder]
+    final_zip_name = f"{STUDENTID}_submission_template.zip"
+    with ZipFile(final_zip_name,'w') as zipp:
+        for path in to_zip:
+            for folderName, subfolders, filenames in os.walk(path):
+                for filename in filenames:
+                    filePath = os.path.join(folderName, filename)
+                    zipp.write(filePath)
+        sample1 = os.path.join('.', 'hw10.md')
+        
+        
+        zipp.write(sample1)
+        
+    print(f"{final_zip_name} file created successfully - submit it through myCourses <3")
+
+
+if __name__ == "__main__":
+    main()
